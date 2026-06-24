5f037e1bbd68e63c3b201d6cbc42fca2d89313b4
Vamshikanagandla <162463008+Vamshikanagandla@users.noreply.github.com>
2026-06-24 23:18:39 +0100

    Update version number in app.py

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ def home():
     <body style="font-family:Arial;text-align:center;padding:60px;background:#f0f4f8">
     <h1 style="color:#1F4E79">CI/CD Pipeline - Live on AWS</h1>
     <p>Automatically deployed using GitHub Actions + Docker + AWS EC2</p>
-    <p>Built by Vamshi Kanagandla Version3.o</p>
+    <p>Built by Vamshi Kanagandla Version34.o</p>
     </body>
     </html>
     '''
-
+ 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
